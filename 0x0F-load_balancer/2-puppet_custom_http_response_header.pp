2ea1ea099a0e5f2472eee4917b969c84a7f7fa9b
maryannemm <maryannemaree@gmail.com>
2024-03-30 02:22:19 -0700

    2 correction

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,24 @@
-exec { 'configure_custom_http_header':
-  command  => 'apt-get -y update;
-               apt-get -y install nginx;
-               sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $::hostname;" /etc/nginx/sites-available/default;
-               service nginx restart',
-  provider => shell,
+# Define a custom fact to get the hostname
+Facter.add('custom_hostname') do
+  setcode 'hostname'
+end
+
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Define a custom Nginx config file
+file { '/etc/nginx/conf.d/custom_headers.conf':
+  ensure  => present,
+  content => "server_tokens off;\nadd_header X-Served-By $::custom_hostname;\n",
+  notify  => Service['nginx'],
+}
+
+# Ensure Nginx service is running
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
 }
 
