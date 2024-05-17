b003cc7b182ac39a1f99ff5fe537c5d30c87b7b5
Maryanne M <40240468+maryannemm@users.noreply.github.com>
2024-05-17 17:42:44 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 
 # Fixing the number of failed requests to get to 0
 exec { 'fix--for-nginx':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  command => "sed -i 's/15/4096/' /etc/nginx/nginx.conf; sudo service nginx restart",
   path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
 
