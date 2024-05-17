3bc74bb148a98e986732a8a7807f073263e8e213
Maryanne M <40240468+maryannemm@users.noreply.github.com>
2024-05-17 16:31:32 +0300

    Update 1-user_limit.pp
    corrections

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,23 +1,4 @@
 # Puppet manifest to adjust file limits for the holberton user
 
-# Change OS configuration for holberton user
-exec { 'change-os-configuration-for-holberton-user':
-  command => '/bin/sed -i "s/^\(session\s\+required\s\+pam_limits\.so\)/#\1/" /etc/pam.d/su',
-  onlyif  => '/bin/grep -q "^session\s\+required\s\+pam_limits\.so" /etc/pam.d/su',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# Apply changes to PAM configuration
-exec { 'apply-pam-changes':
-  command     => '/bin/bash -c "echo -e \"holberton\t-\tnofile\t65536\" >> /etc/security/limits.conf"',
-  unless      => '/bin/grep -q "^holberton\s\+-\s+nofile\s+65536" /etc/security/limits.conf',
-  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  notify      => Exec['reload-pam'],
-}
-
-# Reload PAM configuration
-exec { 'reload-pam':
-  command     => '/sbin/sysctl -p',
-  refreshonly => true,
-}
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
 
